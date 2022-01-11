f7f85f52407062044eaf95cd5a678b750f5cf18e
Rodrigo Alvares <10271378+ralvares@users.noreply.github.com>
2022-01-11 11:40:05 +0400

    Update entrypoint.sh

--- qperf/entrypoint.sh
+++ qperf/entrypoint.sh
@@ -5,7 +5,6 @@ t=10
 
 echo "Running as ${SERVICE_TYPE}"
 echo "SERVER_ADDR ${SERVER_ADDR}"
-echo "SERVER_PORT ${SERVER_PORT}"
 
 
 if [ ${SERVICE_TYPE} == "server" ] ; then
@@ -14,7 +13,7 @@ elif [ ${SERVICE_TYPE} == "qperf" ] ; then
   while true ; do
     sleep 1
     t=$(shuf -i ${PERF_INTERVAL} -n 1)
-      lat=$(qperf -t ${t} --use_bits_per_sec ${SERVER_ADDR} -lp ${SERVER_PORT} tcp_lat | grep "latency")
+      lat=$(qperf -t ${t} --use_bits_per_sec ${SERVER_ADDR} tcp_lat | grep "latency")
     echo "iteration:${iteration}  t:${t} ${lat}"
     let iteration++
   done
@@ -22,7 +21,7 @@ elif [ ${SERVICE_TYPE} == "iperf" ] ; then
   while true ; do
     sleep 1
     t=$(shuf -i ${PERF_INTERVAL} -n 1)
-      iperf3 -t ${t} -i 1 -c ${SERVER_PORT}
+      iperf3 -t ${t} -i 1 -c ${SERVER_ADDR}
     let iteration++
   done
 elif [ ${SERVICE_TYPE} == "ping" ] ; then
